8ae1382c4edbbd8dbcf89ce40b6a4359ca4c44b8
Gurpreet Singh <100257273+Gurpreet-s0@users.noreply.github.com>
2025-11-22 13:27:35 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,97 @@
+let moar = new Audio("Voicy_Loud Moaning.mp3")
+moar.loop = true
+let key1 = document.querySelector("#key1")
+let key2= document.querySelector("#key2")
+let key3 = document.querySelector("#key3")
+let key4 = document.querySelector("#key4")
+let key5 = document.querySelector("#key5")
+let key6 = document.querySelector("#key6")
+let key7 = document.querySelector("#key7")
+let key8 = document.querySelector("#key8")
+let key9= document.querySelector("#key9")
+let key10 = document.querySelector("#key10")
+let key11 = document.querySelector("#key11")
+let key12 = document.querySelector("#key12")
+let key13 = document.querySelector("#key13")
+let key14 = document.querySelector("#key14")
+let key15 = document.querySelector("#key15")
+let key16 = document.querySelector("#key16")
+let key17 = document.querySelector("#key17")
+
+let board = document.querySelector(".board")
+key1.addEventListener("click",function(dets){
+    console.log(dets)
+    moar.play()
+})
+key2.addEventListener("click",function(dets){
+    console.log(dets)
+    moar.play()
+})
+key3.addEventListener("click",function(dets){
+    console.log(dets)
+    moar.play()
+})
+key4.addEventListener("click",function(dets){
+    console.log(dets)
+    moar.play()
+})
+key5.addEventListener("click",function(dets){
+    console.log(dets)
+    moar.play()
+})
+key6.addEventListener("click",function(dets){
+    console.log(dets)
+    moar.play()
+})
+key7.addEventListener("click",function(dets){
+    console.log(dets)
+    moar.play()
+})
+key8.addEventListener("click",function(dets){
+    console.log(dets)
+    moar.play()
+})
+key9.addEventListener("click",function(dets){
+    console.log(dets)
+    moar.play()
+})
+key10.addEventListener("click",function(dets){
+    console.log(dets)
+    moar.play()
+})
+key11.addEventListener("click",function(dets){
+    console.log(dets)
+    moar.play()
+})
+key12.addEventListener("click",function(dets){
+    console.log(dets)
+    moar.play()
+})
+key13.addEventListener("click",function(dets){
+    console.log(dets)
+    moar.play()
+})
+key14.addEventListener("click",function(dets){
+    console.log(dets)
+    moar.play()
+})
+key15.addEventListener("click",function(dets){
+    console.log(dets)
+    moar.play()
+})
+key16.addEventListener("click",function(dets){
+    console.log(dets)
+    moar.play()
+})
+key17.addEventListener("click",function(dets){
+    console.log(dets)
+    moar.play()
+})
+
+
+// for (let i = 1; i < 18; i++){  
+//     `key${i}`.addEventListener("click",function(dets){
+//     console.log(dets)
+//     moar.play()
+// })
+// }
